ffdc830eb5bdc829f04eb43d09d863fb2e6fabe7
Leon Feng <leon@feng.am>
2021-01-21 07:45:51 +0000

    feat(content): add footer content

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,34 @@
 import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
 import { css } from "@emotion/react";
 
-const Footer = ({ gridArea }) => (
-  <footer
-    css={css`
-      grid-area: ${gridArea};
-    `}
-  >
-    <h1>Footer</h1>
-  </footer>
-);
+const Footer = ({ gridArea }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <footer
+      css={css`
+        grid-area: ${gridArea};
+      `}
+    >
+      <hr />
+      <p>
+        &copy; {new Date().getFullYear()} {siteTitle}. All rights reserved.
+        <br />
+        {/* TODO: link to opt-out page */}
+        This site uses Google Analytics. You can opt out at any time.
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
