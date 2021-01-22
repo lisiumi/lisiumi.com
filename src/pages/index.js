b931e39ffad6262e4fa5941948d8f23cdca94ce5
Leon Feng <leon@feng.am>
2021-01-22 13:39:24 +0000

    refactor(index): extract HomeSection component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { css } from "@emotion/react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
+import HomeSection from "../components/home-section";
 import ABlank from "../components/a-blank";
 import AnimatedHeading from "../components/animated-heading";
 import AnimatedParagraph from "../components/animated-paragraph";
@@ -16,75 +17,7 @@ import {
 const IndexPage = ({ data }) => {
   return (
     <>
-      <section
-        css={css`
-          @supports (display: grid) {
-            display: grid;
-            grid-template: 50px auto 50px / 20px auto 20px;
-          }
-        `}
-        id="home"
-      >
-        <Img
-          imgStyle={{ height: "650px" }}
-          style={{
-            gridColumn: "1 / 4",
-            gridRow: "1 / 4",
-            height: "650px",
-          }}
-          fluid={data.aboutImage.childImageSharp.fluid}
-        />
-
-        <div
-          css={css`
-            @supports not (display: grid) {
-              position: relative;
-              top: -560px;
-              @media (max-height: 420px) and (orientation: landscape) {
-                top: -460px;
-              }
-            }
-
-            @supports (display: grid) {
-              grid-column: 2 / 3;
-              grid-row: 2 / 3;
-              place-self: center;
-            }
-            margin: 0 auto;
-            max-width: 1024px;
-            z-index: 10;
-          `}
-        >
-          <h1
-            css={css`
-              color: #facb23;
-              font-size: 3rem;
-              font-weight: 900;
-              text-shadow: 1px 1px 2px black;
-            `}
-          >
-            <AnimatedHeading animationCSS={trackingInExpand}>
-              Static Sites,
-            </AnimatedHeading>
-            <br />
-            <AnimatedHeading animationCSS={trackingInExpand}>
-              Dynamic Powers.
-            </AnimatedHeading>
-          </h1>
-          <AnimatedParagraph
-            animationCSS={textFocusIn}
-            color="white"
-            textShadow="1px 1px 2px black"
-          >
-            Are you tired of slow websites that take half a minute to load on
-            mobile devices? Are you worried about your website getting hacked?
-            Do you suspect you’re paying too much for hosting and maintenance?
-            If you answered “yes” to any of the above questions, you’ll love
-            Lisiumi’s static websites.
-          </AnimatedParagraph>
-        </div>
-      </section>
-
+      <HomeSection />
       <section
         css={css`
           @media (min-width: 500px) {
@@ -299,13 +232,6 @@ export default IndexPage;
 
 export const query = graphql`
   query IndexPageQuery {
-    aboutImage: file(relativePath: { eq: "book-computer-design-326424.jpg" }) {
-      childImageSharp {
-        fluid(maxHeight: 600, quality: 90) {
-          ...GatsbyImageSharpFluid_withWebp_tracedSVG
-        }
-      }
-    }
     uxImage: file(relativePath: { eq: "blur-business-computer-230544.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1024, maxHeight: 440, cropFocus: CENTER) {
