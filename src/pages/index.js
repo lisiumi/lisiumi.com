677f9880b1b22c1d8affc3beb91b216dd9dbba41
Leon Feng <leon@feng.am>
2021-01-22 13:59:42 +0000

    refactor(index): extract ContactSection component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { css } from "@emotion/react";
 import HomeSection from "../components/home-section";
 import WhySection from "../components/why-section";
 import ServicesSection from "../components/services-section";
+import ContactSection from "../components/contact-section";
 
 const IndexPage = () => {
   return (
@@ -10,22 +10,7 @@ const IndexPage = () => {
       <HomeSection />
       <WhySection />
       <ServicesSection />
-      <section
-        css={css`
-          margin: 2rem auto;
-          max-width: 1024px;
-        `}
-        id="contact"
-      >
-        <h2>Contact</h2>
-        <p>
-          Please email{" "}
-          <a href="mailto:&#x63;&#x6F;&#x6D;&#x70;&#x61;&#x6E;&#x79;&#x40;&#x6C;&#x69;&#x73;&#x69;&#x75;&#x6D;&#x69;&#x2E;&#x63;&#x6F;&#x6D;">
-            &#x63;&#x6F;&#x6D;&#x70;&#x61;&#x6E;&#x79;&#x40;&#x6C;&#x69;&#x73;&#x69;&#x75;&#x6D;&#x69;&#x2E;&#x63;&#x6F;&#x6D;
-          </a>
-          .
-        </p>
-      </section>
+      <ContactSection />
     </>
   );
 };
